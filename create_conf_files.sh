6c5af9fc37260ec24164f55302b93c7e5894b5bd
mkenjis <mkenjis@gmail.com>
2023-04-21 07:54:07 -0300

    Added spark master recovery feature

--- create_conf_files.sh
+++ create_conf_files.sh
@@ -2,4 +2,5 @@
 # spark-env.sh (SPARK)
 # ============
 echo 'export JAVA_HOME=/usr/local/jre1.8.0_181' >$SPARK_HOME/conf/spark-env.sh
+echo 'export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=FILESYSTEM -Dspark.deploy.recoveryDirectory=/root/recover"' >>$SPARK_HOME/conf/spark-env.sh
 chmod +x $SPARK_HOME/conf/spark-env.sh
